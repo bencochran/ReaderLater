7f67ae826472e0ec3defd0319d2cdf260f165134
Ben Cochran <bcochran@gmail.com>
2012-07-05 22:32:31 -0500

    Added all the code to do the stuff.

---
+++ ReaderLater/BCReaderLater.h
@@ -0,0 +1,15 @@
+//
+//  BCReaderLater.h
+//  ReaderLater
+//
+//  Created by Ben Cochran on 7/5/12.
+//  Copyright (c) 2012 Ben Cochran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCReaderLater : NSObject
+
++ (BCReaderLater *)sharedInstance;
+
+@end
---
+++ ReaderLater/BCReaderLater.m
@@ -0,0 +1,110 @@
+//
+//  BCReaderLater.m
+//  ReaderLater
+//
+//  Created by Ben Cochran on 7/5/12.
+//  Copyright (c) 2012 Ben Cochran. All rights reserved.
+//
+
+#import "BCReaderLater.h"
+#import "JRSwizzle.h"
+
+@interface BCReaderLater()
++ (NSString *)pluginVersion;
+@end
+
+@implementation BCReaderLater
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        NSError *error = nil;
+        
+        Class c = NSClassFromString(@"BrowserWindowControllerMac");
+        [c jr_swizzleMethod:@selector(toggleReader:) withMethod:@selector(BCReaderLater_toggleReader:) error:&error];
+        if (error) {
+            NSLog(@"Could not swizzle -[BrowserWindowControllerMac toggleReader:]: %@", error);
+        } else {
+            [c jr_swizzleMethod:@selector(updateReaderButton) withMethod:@selector(BCReaderLater_updateReaderButton) error:&error];
+            if (error) {
+                NSLog(@"Could not swizzle -[BrowserWindowControllerMac updateReaderButton]: %@", error);
+            }
+        }
+    }
+    return self;
+}
+
++ (NSString *)pluginVersion
+{
+    return [[[NSBundle bundleForClass:self] infoDictionary] objectForKey:@"CFBundleVersion"];
+}
+
++ (BCReaderLater *)sharedInstance
+{
+    static BCReaderLater *sharedInstance = nil;
+    
+    if (sharedInstance == nil) {
+        sharedInstance = [[BCReaderLater alloc] init];
+    }
+    
+    return sharedInstance;
+}
+
++ (void)load
+{
+    [self sharedInstance];
+    NSLog(@"ReaderLater %@ Loaded", [self pluginVersion]);
+}
+
+@end
+
+
+@interface NSWindowController(BCReaderLater)
+- (void)BCReaderLater_toggleReader:(id)arg1;
+- (void)BCReaderLater_updateReaderButton;
+@end
+
+@implementation NSWindowController(BCReaderLater)
+
+- (void)BCReaderLater_toggleReader:(id)arg1
+{
+    NSString *instapaperBookmarkletString = @"function iprl5(){var d=document,z=d.createElement('scr'+'ipt'),b=d.body,l=d.location;try{if(!b)throw(0);d.title='(Saving...) '+d.title;z.setAttribute('src',l.protocol+'//www.instapaper.com/j/ZkKUqxBKbJMs?u='+encodeURIComponent(l.href)+'&t='+(new Date().getTime()));b.appendChild(z);}catch(e){alert('Please wait until the page has loaded.');}}iprl5();void(0)";
+    
+    id browserDocument = [self valueForKey:@"_document"];
+    if ([browserDocument respondsToSelector:@selector(evaluateJavaScript:)]) {
+        [browserDocument performSelector:@selector(evaluateJavaScript:) withObject:instapaperBookmarkletString];
+    } else {
+        [self BCReaderLater_toggleReader:arg1];
+    }
+}
+
+- (void)BCReaderLater_updateReaderButton
+{
+    [self BCReaderLater_updateReaderButton];
+    
+    id readerButton = [[self valueForKey:@"_toolbarController"] performSelector:@selector(readerButton)];
+    
+    SEL stateSelector = @selector(setReaderButtonState:animate:);
+    if ([readerButton respondsToSelector:stateSelector]) {
+        // Always make the button active (state "2" apparently)
+        // In the future, this should check to make sure the page is loaded
+        // and set the state accordingly, but always active will do for now.
+        NSMethodSignature *methodSignature = [[readerButton class] instanceMethodSignatureForSelector:stateSelector];
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+        
+        [invocation setSelector:stateSelector];
+        [invocation setTarget:readerButton];
+        NSInteger state = 2;
+        BOOL animate = NO;
+        [invocation setArgument:&state atIndex:2];
+        [invocation setArgument:&animate atIndex:3];
+        [invocation invoke];
+    }
+    
+    if ([readerButton respondsToSelector:@selector(setTitle:)]) {
+        [readerButton setTitle:NSLocalizedString(@"Later", @"The replacement button title.")];
+    }
+}
+
+@end
